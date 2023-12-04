6e423228f1e59b116c4ee8c6336f0eda6ff6337a
Maksim.Buryshynets <maksim.buryshynets.ext@vc-trade.de>
2023-12-04 09:53:59 +0100

    Day 02 - refactored;

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,20 +1,11 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.asSequence()
-            .mapNotNull { pattern.find(it) }
-            .map { it.destructured }
-            .map { (id, seq) -> Game(id = id.toInt(), subsets = pasrseSubset(seq)) }
-            .filter { it.subsets.all(Subset::valid) }
-            .sumOf { it.id }
-    }
+    fun part1(input: List<String>): Int = parseInput(input)
+        .filter { it.subsets.all(Subset::valid) }
+        .sumOf { it.id }
 
-    fun part2(input: List<String>): Int {
-        return input.asSequence()
-            .mapNotNull { pattern.find(it) }
-            .map { it.destructured }
-            .map { (id, seq) -> Game(id = id.toInt(), subsets = pasrseSubset(seq)) }
-            .sumOf { it.subsets.maxOf { it.r } * it.subsets.maxOf { it.g } * it.subsets.maxOf { it.b } }
-    }
+    fun part2(input: List<String>): Int = parseInput(input)
+        .map { it.subsets }
+        .sumOf { it.maxOf(Subset::r) * it.maxOf(Subset::g) * it.maxOf(Subset::b) }
 
     val input = readInput("Day02")
 
@@ -22,30 +13,45 @@ fun main() {
     part2(input).println()
 }
 
-private fun pasrseSubset(text: String): List<Subset> {
-    return text.split(";").map(String::trim).map { single ->
-        single.split(",").map(String::trim).associate {
-            it.split(" ").let { (value, color) ->
-                color to value
-            }
+private fun parseInput(input: List<String>): Sequence<Game> {
+    return input.asSequence()
+        .mapNotNull { pattern.find(it) }
+        .map { it.destructured }
+        .map { (num, seq) ->
+            Game(
+                id = num.toInt(),
+                subsets = parseSubset(seq)
+            )
         }
-    }.map {
-        Subset(
-            r = it["red"]?.toInt() ?: 0,
-            g = it["green"]?.toInt() ?: 0,
-            b = it["blue"]?.toInt() ?: 0,
-        )
-    }
 }
+
+private fun parseSubset(text: String): List<Subset> =
+    text.split(";")
+        .map(String::trim)
+        .map { data ->
+            data.split(",")
+                .map(String::trim)
+                .associate {
+                    it.split(" ").let { (value, color) ->
+                        color to value
+                    }
+                }
+        }
+        .map {
+            Subset(
+                r = it["red"]?.toInt() ?: 0,
+                g = it["green"]?.toInt() ?: 0,
+                b = it["blue"]?.toInt() ?: 0,
+            )
+        }
 
 private data class Game(val id: Int, val subsets: List<Subset>)
-private data class Subset(val r: Int, val g: Int, val b: Int) {
-    val valid: Boolean
-        get() {
-            return r <= 12
-                && g <= 13
-                && b <= 14
-        }
-}
+private data class Subset(val r: Int, val g: Int, val b: Int)
 
 private val pattern = "^Game (?<num>\\d+): (?<seq>.+)$".toRegex()
+private val Subset.valid: Boolean
+    get() {
+        return r <= 12
+            && g <= 13
+            && b <= 14
+    }
