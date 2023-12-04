d1e8b6cb3fa3a3f7311210a765eae2eae7150904
Maksim.Buryshynets <maksim.buryshynets.ext@vc-trade.de>
2023-12-04 17:27:38 +0100

    Day 03 - partly;

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,4 +1,8 @@
+import java.util.LinkedList
+
 fun main() {
+    data class ValueHolder(val value: Int, val gear: Point)
+
     val input = readInput("Day03")
 
     fun part1(): Int {
@@ -25,7 +29,38 @@ fun main() {
         return result
     }
 
-    fun part2(): Int = TODO()
+    fun part2(): Int {
+        var result = LinkedList<ValueHolder>()
+        lines@ for ((index, line) in input.withIndex()) {
+            matching@ for (matching in pattern.findAll(line)) {
+                val yFrom = (index - 1).coerceAtLeast(0)
+                val yUpto = (index + 1).coerceAtMost(input.size - 1)
+                val xFrom = (matching.range.first - 1).coerceAtLeast(0)
+                val xUpto = (matching.range.last + 1).coerceAtMost(line.length - 1)
+                var valid = false
+                var point = Point(-1, -1)
+                for (y in (yFrom..yUpto)) {
+                    for (x in (xFrom..xUpto)) {
+                        val value = input[y][x]
+                        if (!value.isDigit() && value != '.') {
+                            valid = true
+                            if (value == '*') {
+                                point = Point(x, y)
+                            }
+                        }
+                    }
+                }
+                if (valid) {
+                    result += ValueHolder(matching.value.toInt(), point)
+                }
+            }
+        }
+        return result
+            .groupBy { it.gear }
+            .filter { it.key != Point(-1, -1) && it.value.size == 2 }
+            .map { it.value.map { it.value }.fold(1) { prev, next -> prev * next } }
+            .sum()
+    }
 
     part1().println()
     part2().println()
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,6 +2,7 @@ import java.math.BigInteger
 import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
+import kotlin.math.abs
 
 /**
  * Reads lines from the given input txt file.
@@ -19,3 +20,43 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+
+data class Point(val x: Int, val y: Int) : Comparable<Point> {
+
+    override fun compareTo(other: Point): Int = COMPARATOR.compare(this, other)
+
+    fun adjacentPoints(diagonal: Boolean = true) = sequence {
+        yield(value = copy(y = y + 1))
+        if (diagonal) yield(value = copy(x = x + 1, y = y + 1))
+        yield(value = copy(x = x + 1))
+        if (diagonal) yield(value = copy(x = x + 1, y = y - 1))
+        yield(value = copy(y = y - 1))
+        if (diagonal) yield(value = copy(x = x - 1, y = y - 1))
+        yield(value = copy(x = x - 1))
+        if (diagonal) yield(value = copy(x = x - 1, y = y + 1))
+    }
+
+    fun enclosedTo(other: Point): Point = when {
+        x == other.x && y == other.y -> this
+
+        x == other.x && y < other.y -> copy(y = y + 1)
+        y == other.y && x < other.x -> copy(x = x + 1)
+
+        y < other.y && x < other.x -> copy(y = y + 1, x = x + 1)
+
+        x == other.x -> copy(y = y - 1)
+        y == other.y -> copy(x = x - 1)
+
+        y < other.y -> copy(y = y + 1, x = x - 1)
+        x < other.x -> copy(y = y - 1, x = x + 1)
+
+        else -> copy(y = y - 1, x = x - 1)
+    }
+
+    fun manhattanDistanceTo(other: Point) = abs(x - other.x) + abs(y - other.y)
+
+    companion object {
+        val COMPARATOR: Comparator<Point> = compareBy({ it.y }, { it.x })
+    }
+}
