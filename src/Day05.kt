f3f925cda9c0b51cb5060d9afb48a428f3dbab23
Maksim.Buryshynets <maksim.buryshynets.ext@vc-trade.de>
2023-12-06 16:53:54 +0100

    Day 05 - parser;

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,8 +1,10 @@
 fun main() {
     data class Value(val type: String, val data: List<Long>)
-    data class Descriptor(val rangeInto: Long, val rangeFrom: Long, val rangeSize: Long)
-    data class Mapping(val from: String, val into: String, val data: List<Descriptor>)
+    data class Range(val rangeInto: Long, val rangeFrom: Long, val rangeSize: Long)
+    data class Mapping(val from: String, val into: String, val data: List<Range>)
     data class Model(val initial: Value, val mapping: List<Mapping>)
+
+    val pattern = "^(?<from>\\w+)-to-(?<into>\\w+) map:$".toRegex()
 
     val data = readInput("Day05").split("").destruct().let { (head, tail) ->
         Model(
@@ -12,13 +14,13 @@ fun main() {
                     data = data.numbers()
                 )
             },
-            mapping = tail.map { line ->
-                pattern.find(line.first())!!.groups.let { match ->
+            mapping = tail.map { it.destruct() }.map { (head, tail) ->
+                pattern.find(head)!!.groups.let { match ->
                     Mapping(
                         from = match["from"]!!.value,
                         into = match["into"]!!.value,
-                        data = line.drop(1).map(String::numbers).map { (one, two, thr) ->
-                            Descriptor(
+                        data = tail.map(String::numbers).map { (one, two, thr) ->
+                            Range(
                                 rangeInto = one,
                                 rangeFrom = two,
                                 rangeSize = thr,
@@ -38,5 +40,3 @@ fun main() {
     part1().println()
     part2().println()
 }
-
-private val pattern = "^(?<from>\\w+)-to-(?<into>\\w+) map:$".toRegex()
