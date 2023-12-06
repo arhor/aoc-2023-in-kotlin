c06de49fbe9cee1bac26d0d8431c9798f90aecbe
Maksim.Buryshynets <maksim.buryshynets.ext@vc-trade.de>
2023-12-06 16:36:55 +0100

    Day 05 - parser;

---
+++ src/Day05.kt
@@ -0,0 +1,47 @@
+fun main() {
+    data class Value(
+        val type: String,
+        val data: List<Long>,
+    )
+
+    data class Mapping(
+        val from: String,
+        val into: String,
+        val data: List<List<Long>>,
+    )
+
+    data class Model(
+        val initial: Value,
+        val mapping: List<Mapping>,
+    )
+
+    val data = readInput("Day05").split("").let { lines ->
+        Model(
+            initial = lines.first().single().split(":").let { (type, data) ->
+                Value(
+                    type = type,
+                    data = data.split(" ").filter(String::isNotEmpty).map(String::toLong)
+                )
+            },
+            mapping = lines.drop(1).map { line ->
+                pattern.find(line.first())!!.groups.let { match ->
+                    Mapping(
+                        from = match["from"]!!.value,
+                        into = match["into"]!!.value,
+                        data = line.drop(1).map { it.split(" ").map(String::toLong) }
+                    )
+                }
+            }
+        )
+    }
+
+    println(data)
+
+    fun part1(): Int = 0
+    fun part2(): Int = 0
+
+    part1().println()
+    part2().println()
+}
+
+private val pattern = "^(?<from>\\w+)-to-(?<into>\\w+) map:$".toRegex()
