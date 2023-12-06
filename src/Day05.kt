e808f61a52c0155b66afcf5e04d02cad5a0be38a
Maksim.Buryshynets <maksim.buryshynets.ext@vc-trade.de>
2023-12-06 17:03:00 +0100

    Day 05 - parser;

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,12 +1,12 @@
 fun main() {
     data class Value(val type: String, val data: List<Long>)
     data class Range(val rangeInto: Long, val rangeFrom: Long, val rangeSize: Long)
-    data class Mapping(val from: String, val into: String, val data: List<Range>)
-    data class Model(val initial: Value, val mapping: List<Mapping>)
+    data class Mapping(val from: String, val into: String, val ranges: List<Range>)
+    data class Model(val initial: Value, val mappings: List<Mapping>)
 
     val pattern = "^(?<from>\\w+)-to-(?<into>\\w+) map:$".toRegex()
 
-    val data = readInput("Day05").split("").destruct().let { (head, tail) ->
+    val model = readInput("Day05").split("").destruct().let { (head, tail) ->
         Model(
             initial = head.single().split(":").let { (type, data) ->
                 Value(
@@ -14,12 +14,12 @@ fun main() {
                     data = data.numbers()
                 )
             },
-            mapping = tail.map { it.destruct() }.map { (head, tail) ->
+            mappings = tail.map { it.destruct() }.map { (head, tail) ->
                 pattern.find(head)!!.groups.let { match ->
                     Mapping(
                         from = match["from"]!!.value,
                         into = match["into"]!!.value,
-                        data = tail.map(String::numbers).map { (one, two, thr) ->
+                        ranges = tail.map(String::numbers).map { (one, two, thr) ->
                             Range(
                                 rangeInto = one,
                                 rangeFrom = two,
@@ -32,9 +32,13 @@ fun main() {
         )
     }
 
-    println(data)
-
-    fun part1(): Int = 0
+    fun part1(): Int {
+        var result = model.initial.data
+        for (mapping in model.mappings) {
+            println(mapping)
+        }
+        return 0
+    }
     fun part2(): Int = 0
 
     part1().println()
